7cebcc494da39c9e07c415e8350e0b5395c65b89
kgaolodevs <kgaolodev@gmail.com>
2020-06-30 19:08:58 +0200

    Added active pad onClick functionality

--- app.js
+++ app.js
@@ -6,21 +6,38 @@ class DrumKit {
     this.hihatAudio = document.querySelector(".app__hihatSound");
     this.percAudio = document.querySelector(".app__percSound");
     this.shakerAudio = document.querySelector(".app__shakerSound");
+    this.playButton = document.querySelector(".app__playBtn");
     this.index = 0;
+    this.bpm = 140;
+  }
+
+  activePad() {
+    this.classList.toggle("active");
   }
 
   repeat() {
     let step = this.index % 8; //when step's value equals to 8, it will reset to 0
-    console.log(step);
+    let activeBars = document.querySelectorAll(`.beat${step}`);
+    console.log(activeBars);
     this.index++;
   }
 
   start() {
+    const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
       this.repeat();
-    }, 1000);
+    }, interval);
   }
 }
 
+// Objects
 const drumKit = new DrumKit();
-// drumKit.start();
+
+// Event Listeners
+drumKit.playButton.addEventListener("click", () => {
+  drumKit.start();
+});
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+});
